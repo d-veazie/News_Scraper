79f3b52a0886c0e31db4fd300ad31d97b82cac88
Valentino <veaziedallen@gmail.com>
2019-10-20 17:02:32 -0600

    changed folder structure, added most code

--- server.js
+++ server.js
@@ -1,13 +1,87 @@
-let express = require('express');
-let mongoose = require('mongoose');
-let logger = require('morgan');
-let axios = require('axios');
-let cheerio = require('cheerio');
-let db = require('./models')
+let express = require("express");
+let mongoose = require("mongoose");
+let logger = require("morgan");
+let axios = require("axios");
+let cheerio = require("cheerio");
+let db = require("./models");
 let PORT = 6969;
 let app = express();
 
 app.use(logger("dev"));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/NewsScrape", { useNewUrlParser: true });
+
+// Routes
+
+app.get("/scrape", function(req, res) {
+  axios
+    .get("https://www.technewsworld.com/perl/section/internet/")
+    .then(function(response) {
+      var $ = cheerio.load(response.data);
+
+      $("td a").each(function(i, element) {
+        var result = {};
+
+        result.title = $(this).text();
+        result.link = $(this).attr("href");
+
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      });
+
+      res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
